f68ce35c6b09e08a4bcaa3a613e03edf22b7dcc8
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2019-05-23 10:35:12 +0400

    increment, toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false)
+  const incrementCount = () => {
+   //setCount(count+1);
+    setCount(prevCount=> prevCount+1)
+  }
+
+  const toggleLight = () => {
+    setIsOn(prevIsOn=> !prevIsOn)
+  }
+
+  return (
+    <>
+
+    <button onClick={incrementCount}> i was cliced {count} times </button>
+      <div onClick={toggleLight}>{isOn ? 'да' : 'нет'}</div>
+      </>
+      )
+}
+
+
+/*function App() {
   return (
     <div> test</div>
   );
-}
+}*/
 
 export default App;
