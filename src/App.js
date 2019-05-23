906389052f4d3c5b257a8fad595c700aada4b9ca
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2019-05-23 10:46:13 +0400

    add efect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false)
+  const [mousePosition, setMousePosition] = useState({x: null, y: null})
+
+  useEffect(()=>{
+    document.title = `you a cliked ${count}`
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return ()=> {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+  },[count])
+
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY
+    })
+  }
+
+
   const incrementCount = () => {
    //setCount(count+1);
     setCount(prevCount=> prevCount+1)
@@ -13,11 +32,16 @@ const App = () => {
     setIsOn(prevIsOn=> !prevIsOn)
   }
 
+
+
   return (
     <>
 
     <button onClick={incrementCount}> i was cliced {count} times </button>
       <div onClick={toggleLight}>{isOn ? 'да' : 'нет'}</div>
+
+      <h2>Mouse postion</h2>
+      {JSON.stringify(mousePosition, null, 2)}
       </>
       )
 }
